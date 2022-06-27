b11df19589754722522a697c0efcf91bb9467ff9
imkrish513 <70lionsct@gmail.com>
2022-06-27 09:32:16 -0400

    HomeScreen

---
+++ Src/Screens/Home/HomeScreen.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+const HomeScreen = ()=> {
+  return (
+  <view>
+
+    
+  </view>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#c4f3ff'
+  },
+})
+  
+export default HomeScreen
