17f1486476e21cdf7b950aed648c40363e47f070
shreyas2692 <108104375+shreyas2692@users.noreply.github.com>
2022-06-27 22:26:47 -0400

    f

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import SignInScreen from './Src/Screens/Sign_In/SignInScreen';
 import SignUpScreen from './Src/Screens/Sign_Up/SignUpScreen';
 import HomeScreen from './Src/Screens/Home/HomeScreen'
+import BusCode from './Src/Screens/Home/BusCode'
 const App = ()=> {
   return (
     <SafeAreaView style={styles.container}>
-     <HomeScreen />
+     <BusCode />
     </SafeAreaView>
   );
 }
