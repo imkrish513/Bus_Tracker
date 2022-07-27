22d665d20339b25d6d5c3ca3b43d0301af1d64a9
imkrish513 <70lionsct@gmail.com>
2022-07-26 21:50:30 -0400

    AWS Amplify

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
-
+import { Amplify } from 'aws-amplify'
+import awsconfig from './Src/aws-exports'
 import Navigation from './Src/Navigation';
+
+
+Amplify.configure(awsconfig)
+
+
 
 
 
